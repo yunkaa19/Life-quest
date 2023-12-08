a4127ac2f69e83bb3c821bc3506399d60a31786c
Gerven,Joery J.A.G. van <joery.vangerven@student.fontys.nl>
2023-12-08 15:10:07 +0100

    Adding Jigsaw puzzle

---
+++ Life-Quest/Assets/Scripts/Jigsaw Scripts/DragAndDrop.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class DragAndDrop : MonoBehaviour
+{
+    public GameObject SelectedPiece;
+    int OIL = 1;
+    
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+            if(hit.transform.CompareTag("Puzzle"))
+            {
+                if(!hit.transform.GetComponent<PiecesScript>().InRightPosition)
+                {
+                    SelectedPiece = hit.transform.gameObject;
+                    SelectedPiece.GetComponent<PiecesScript>().Selected = true;
+                    SelectedPiece.GetComponent<SortingGroup>().sortingOrder = OIL;
+                    OIL++;
+                }
+
+            }    
+        }
+        if(Input.GetMouseButtonUp(0))
+        {
+            if(SelectedPiece != null)
+            {
+                SelectedPiece.GetComponent<PiecesScript>().Selected = false;
+                SelectedPiece = null;
+            }
+        }
+
+        if(SelectedPiece != null)
+        {
+            Vector3 MousePoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            SelectedPiece.transform.position = new Vector3(MousePoint.x,MousePoint.y,0);
+        }
+    }
+}
---
+++ Life-Quest/Assets/Scripts/Jigsaw Scripts/PiecesScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class PiecesScript : MonoBehaviour
+{
+    private Vector3 RightPosition;
+    public bool InRightPosition;
+    public bool Selected;
+    void Start()
+    {
+        RightPosition = transform.position;
+        transform.position = new Vector3(Random.Range(-1.8f,1.55f), Random.Range(-0.55f,-3.9f));
+    }
+
+    void Update()
+    {
+        if(Vector3.Distance(transform.position, RightPosition) <0.5f)
+        {
+            if(!Selected)
+            {  
+                if(InRightPosition == false)
+                {
+                    transform.position = RightPosition;
+                    InRightPosition = true;
+                    GetComponent<SortingGroup>().sortingOrder = 0;
+                }
+            }
+        }
+    }
+}
