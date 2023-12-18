ee0698216c85696f85ee048cc6a6c19c4f658b4f
DaanvanReisen <54912221+DaanvanReisen@users.noreply.github.com>
2023-12-18 12:08:43 +0100

    Changed game aesthetics

--- Life-Quest/Assets/Scripts/collectTreasure.cs
+++ Life-Quest/Assets/Scripts/collectTreasure.cs
@@ -4,19 +4,13 @@ using UnityEngine;
 
 public class collectTreasure : MonoBehaviour
 {
-
-    void Update()
-    {
-        
-    }
-
     void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log("hit a " + collision.gameObject.tag);
+        Handheld.Vibrate();
         if(collision.gameObject.tag == "Treasure")
         {
             Destroy(collision.gameObject);
-            Handheld.Vibrate();
             Debug.Log("You found it");
         }
     }
--- Life-Quest/Assets/Scripts/spawnTreasure.cs
+++ Life-Quest/Assets/Scripts/spawnTreasure.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class spawnTreasure : MonoBehaviour
 {
+    public float treasureDistance;
     public float horizontalPosition;
     public float verticalPosition;
+    public float verticalPositionStoring;
+    public float verticalAbsolute;
     
     void Start()
     {
@@ -16,18 +19,25 @@ public class spawnTreasure : MonoBehaviour
 
     void DeterminePosition()
     {
-        horizontalPosition = Random.Range(-6.5f, 7.3f);
-        verticalPosition = Random.Range(-19.2f, 16.4f);
-        if(horizontalPosition > -3f && horizontalPosition < 3.5f && verticalPosition > -6f && verticalPosition < 5.5f)
-        {
-            DeterminePosition();
-            return;
-        }
+        treasureDistance = Random.Range(20f,30f);
+        horizontalPosition = Random.Range(-treasureDistance, treasureDistance);
+        verticalPositionStoring = treasureDistance*treasureDistance - horizontalPosition*horizontalPosition;
+        verticalPosition = Mathf.Sqrt(verticalPositionStoring);
+        verticalAbsolute = Random.Range(-1f,1f);
 
-        else
+        if(verticalAbsolute <= 0)
+        {
+            transform.position = new Vector3(horizontalPosition,-verticalPosition,transform.position.z);
+        }
+        if(verticalAbsolute > 0)
         {
             transform.position = new Vector3(horizontalPosition,verticalPosition,transform.position.z);
         }
+
+        
+        
+
+        
     }
 
 }
